cca871d4e4019ac64a7b1a466607467db7181eda
FirstZverev <46249639+FirstZverev@users.noreply.github.com>
2021-03-11 11:48:53 +0300

    Initial commit

---
+++ TestKurs/TestKursApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestKursApp.swift
+//  TestKurs
+//
+//  Created by Володя Зверев on 11.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestKursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
